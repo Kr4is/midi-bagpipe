73f4ee9190e81f0b8e98f26d31fcca073d50aeec
Kr4is <sr.brunocabado@gmail.com>
2023-02-27 21:07:46 +0100

    Add bagpipe device inspection

--- bagpipe-app.py
+++ bagpipe-app.py
@@ -1,6 +1,8 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
 import pygame.midi
 import time
-import os
 import fluidsynth
 
 fs = fluidsynth.Synth()
@@ -8,20 +10,27 @@ fs.start(driver="alsa")
 fs.setting('synth.gain', 0.2)
 sfid = fs.sfload("galician-bagpipe.sf2")
 fs.program_select(0, sfid, 0, 0)
-last_note = None
 
-def printMIDIDeviceList():
-        for i in range(pygame.midi.get_count()):
-            print(pygame.midi.get_device_info(i), i)
+def getBagpipeMidiDevice():
+    pygame.midi.init()
+    for device_index in range(pygame.midi.get_count()):
+        (_, name, input, _, _) = pygame.midi.get_device_info(device_index)
+
+        if ("Arduino Leonardo" in str(name)) and input:
+            return device_index
+    
+    pygame.midi.quit()
+    return None
 
 pygame.init()
-pygame.midi.init()
 
-printMIDIDeviceList()
-
-print("The default input device number is "  + str(pygame.midi.get_default_input_id()))
-input_id = int(pygame.midi.get_default_input_id())
-input_id = 3
+while True:
+    input_id = getBagpipeMidiDevice()
+    if input_id is None:
+        print("Bagpipe MIDI device not found. waiting for device...")
+        time.sleep(0.1)
+    else:
+        break
 
 clock = pygame.time.Clock()
 crashed = False
@@ -34,8 +43,9 @@ while (crashed == False):
     if readed != []:
         on = (readed[0][0][0] == 144)
         note = readed[0][0][1]
-        # print(f"note {note} on {on}")
+        velocity = readed[0][0][2]
+        print(f"note {note} on {on} at velocity {velocity}")
         if (on):
-            fs.noteon(0, note, 60)
+            fs.noteon(0, note, 127)
         else:
             fs.noteoff(0, note)
